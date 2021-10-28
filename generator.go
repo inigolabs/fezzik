49f475f2d34a06344ce55a4ef7e77ab48d94c396
Eitan Joffe <ejoffe@gmail.com>
2021-10-27 22:48:25 -0700

    generate testify/mocks using the mockery library
    commit-id:b86e89d2

--- generator.go
+++ generator.go
@@ -2,11 +2,13 @@ package fezzik
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"html/template"
 	"io"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/inigolabs/fezzik/common"
@@ -17,6 +19,9 @@ import (
 	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/tools/imports"
+
+	mockery "github.com/vektra/mockery/v2/pkg"
+	mockery_config "github.com/vektra/mockery/v2/pkg/config"
 )
 
 func Generate(cfg *Config) {
@@ -37,6 +42,9 @@ func Generate(cfg *Config) {
 	schema, err := parseSchema(schemaBuilder.String())
 	check(err)
 
+	if cfg.PackageName == "mocks" {
+		panic("PackageName cannot be 'mocks'")
+	}
 	genPath := filepath.Join(cfg.PackageDir, cfg.PackageName)
 	err = os.MkdirAll(genPath, os.ModePerm)
 	check(err)
@@ -70,6 +78,7 @@ func Generate(cfg *Config) {
 	check(err)
 	templateStr = strings.ReplaceAll(clientTemplate, "~~", "`")
 	generate(templateStr, visitor.info, writer)
+	generateMocks()
 }
 
 func parseSchema(schemaString string) (*ast.Document, error) {
@@ -129,6 +138,54 @@ func generate(templateStr string, info interface{}, writer io.Writer) {
 	}
 	_, err = writer.Write(formatted)
 	check(err)
+}
+
+func generateMocks() {
+	cfg := mockery_config.Config{
+		All:            true,
+		Case:           "underscore",
+		Dir:            ".",
+		LogLevel:       "error",
+		Outpkg:         "mocks",
+		Output:         "gen/mocks",
+		UnrollVariadic: true,
+	}
+
+	osp := &mockery.FileOutputStreamProvider{
+		Config:                    cfg,
+		BaseDir:                   cfg.Output,
+		InPackage:                 cfg.InPackage,
+		TestOnly:                  cfg.TestOnly,
+		Case:                      cfg.Case,
+		KeepTree:                  cfg.KeepTree,
+		KeepTreeOriginalDirectory: cfg.Dir,
+		FileName:                  cfg.FileName,
+	}
+
+	visitor := &mockery.GeneratorVisitor{
+		Config:            cfg,
+		InPackage:         cfg.InPackage,
+		Note:              cfg.Note,
+		Boilerplate:       "",
+		Osp:               osp,
+		PackageName:       cfg.Outpkg,
+		PackageNamePrefix: cfg.Packageprefix,
+		StructName:        cfg.StructName,
+	}
+
+	walker := mockery.Walker{
+		Config:    cfg,
+		BaseDir:   cfg.Dir,
+		Recursive: true,
+		Filter:    regexp.MustCompile(".*"),
+		LimitOne:  false,
+	}
+
+	ctx := context.Background()
+	generated := walker.Walk(ctx, visitor)
+	if !generated {
+		panic("mocks not generated")
+	}
 }
 
 func check(err error) {
