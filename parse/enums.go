2a595056dcc0bcba6af596f2ac5a535b0f506086
Eitan Joffe <ejoffe@gmail.com>
2021-11-30 14:38:53 -0800

    improved input and enum parser
    commit-id:59e4bfa4

--- parse/enums.go
+++ parse/enums.go
@@ -1,52 +1,39 @@
 package parse
 
 import (
-	"errors"
-
 	"github.com/inigolabs/fezzik/config"
 	"github.com/inigolabs/fezzik/fezzik_ast"
 	"github.com/jensneuse/graphql-go-tools/pkg/ast"
-	"github.com/jensneuse/graphql-go-tools/pkg/astvisitor"
-	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
-	"github.com/rs/zerolog/log"
 )
 
 func ParseEnums(cfg *config.Config, schema *ast.Document, doc *fezzik_ast.Document) error {
-	walker := astvisitor.NewWalker(48)
-	visitor := &enumVisitor{
-		Walker: &walker,
+	parser := &enumParser{
 		config: cfg,
 		schema: schema,
 		doc:    doc,
 	}
-	walker.RegisterEnterEnumTypeDefinitionVisitor(visitor)
-	report := &operationreport.Report{}
-	visitor.Walker.Walk(visitor.schema, visitor.schema, report)
-	if report.HasErrors() {
-		log.Error().Str("errors", report.Error()).Msg("")
-		return errors.New(report.Error())
+	for ref := range schema.EnumTypeDefinitions {
+		parser.ParseEnumTypeDefinition(ref)
 	}
 	return nil
 }
 
-type enumVisitor struct {
-	*astvisitor.Walker
-
+type enumParser struct {
 	config *config.Config
 	schema *ast.Document
 
 	doc *fezzik_ast.Document
 }
 
-func (v *enumVisitor) EnterEnumTypeDefinition(ref int) {
+func (p *enumParser) ParseEnumTypeDefinition(ref int) {
 	enumType := fezzik_ast.EnumType{
-		Name: v.schema.EnumTypeDefinitionNameString(ref),
+		Name: p.schema.EnumTypeDefinitionNameString(ref),
 	}
-	refs := v.schema.EnumTypeDefinitions[ref].EnumValuesDefinition.Refs
+	refs := p.schema.EnumTypeDefinitions[ref].EnumValuesDefinition.Refs
 	for _, valueRef := range refs {
 		enumType.Values = append(enumType.Values,
-			v.schema.EnumValueDefinitionNameString(valueRef))
+			p.schema.EnumValueDefinitionNameString(valueRef))
 	}
 
-	v.doc.EnumTypes[enumType.Name] = enumType
+	p.doc.EnumTypes[enumType.Name] = enumType
 }
--- parse/inputs.go
+++ parse/inputs.go
@@ -1,65 +1,68 @@
 package parse
 
 import (
-	"errors"
+	"fmt"
 
 	"github.com/inigolabs/fezzik/config"
 	"github.com/inigolabs/fezzik/fezzik_ast"
 	"github.com/jensneuse/graphql-go-tools/pkg/ast"
-	"github.com/jensneuse/graphql-go-tools/pkg/astvisitor"
-	"github.com/jensneuse/graphql-go-tools/pkg/operationreport"
-	"github.com/rs/zerolog/log"
 )
 
 func ParseInputs(cfg *config.Config, schema *ast.Document, doc *fezzik_ast.Document) error {
-	walker := astvisitor.NewWalker(48)
-	visitor := &inputVisitor{
-		Walker: &walker,
+	parser := &inputParser{
 		config: cfg,
 		schema: schema,
 		doc:    doc,
 	}
-	walker.RegisterEnterInputObjectTypeDefinitionVisitor(visitor)
-	report := &operationreport.Report{}
-	visitor.Walker.Walk(visitor.schema, visitor.schema, report)
-	if report.HasErrors() {
-		log.Error().Str("errors", report.Error()).Msg("")
-		return errors.New(report.Error())
+	for ref := range schema.InputObjectTypeDefinitions {
+		parser.FirstPassInputObjectTypeDefinition(ref)
+	}
+	for ref := range schema.InputObjectTypeDefinitions {
+		parser.SecondPassInputObjectTypeDefinition(ref)
 	}
 	return nil
 }
 
-type inputVisitor struct {
-	*astvisitor.Walker
-
+type inputParser struct {
 	config *config.Config
 	schema *ast.Document
 
 	doc *fezzik_ast.Document
 }
 
-func (v *inputVisitor) EnterInputObjectTypeDefinition(ref int) {
-	name := v.schema.InputObjectTypeDefinitionNameString(ref)
+func (p *inputParser) FirstPassInputObjectTypeDefinition(ref int) {
+	name := p.schema.InputObjectTypeDefinitionNameString(ref)
+	p.doc.InputTypes[name] = fezzik_ast.InputType{
+		Name: name,
+	}
+}
+
+func (p *inputParser) SecondPassInputObjectTypeDefinition(ref int) {
+	name := p.schema.InputObjectTypeDefinitionNameString(ref)
 	inputType := fezzik_ast.InputType{
 		Name: name,
 	}
 
-	for _, fieldRef := range v.schema.InputObjectTypeDefinitions[ref].InputFieldsDefinition.Refs {
-		fieldName := v.schema.InputValueDefinitionNameString(fieldRef)
-		typeRef := v.schema.InputValueDefinitionType(fieldRef)
-		typeInfo := fezzik_ast.GetTypeInfo(typeRef, v.schema)
+	for _, fieldRef := range p.schema.InputObjectTypeDefinitions[ref].InputFieldsDefinition.Refs {
+		fieldName := p.schema.InputValueDefinitionNameString(fieldRef)
+		typeRef := p.schema.InputValueDefinitionType(fieldRef)
+		typeInfo := fezzik_ast.GetTypeInfo(typeRef, p.schema)
 
 		inputField := fezzik_ast.InputField{
 			Name: fieldName,
 			Type: typeInfo,
 		}
-		if goType, ok := fezzik_ast.GetGoType(v.config, typeInfo.Name); ok {
+
+		if goType, ok := fezzik_ast.GetGoType(p.config, typeInfo.Name); ok {
 			inputField.TypeName = goType
-		} else {
+		} else if _, found := p.doc.EnumTypes[typeInfo.Name]; found {
 			inputField.TypeName = typeInfo.Name
+		} else if _, found := p.doc.InputTypes[typeInfo.Name]; found {
+			inputField.TypeName = typeInfo.Name
+		} else {
+			panic(fmt.Errorf("unknown type '%s' not found in config.ScalarTypeMap ", typeInfo.Name))
 		}
 		inputType.Fields = append(inputType.Fields, inputField)
 	}
-
-	v.doc.InputTypes = append(v.doc.InputTypes, inputType)
+	p.doc.InputTypes[inputType.Name] = inputType
 }
