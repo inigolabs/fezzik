52f862f87e5991c00f461cb889dd15935272d0dc
Nikolai Kaploniuk <nkaploniuk@inigo.io>
2022-11-15 13:03:26 -0800

    use external types for scalars
    commit-id:13291b5d

--- fezzik_ast/types.go
+++ fezzik_ast/types.go
@@ -2,6 +2,7 @@ package fezzik_ast
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/inigolabs/fezzik/config"
 )
@@ -18,11 +19,26 @@ func GetGoType(cfg *config.Config, typeName string) (string, bool) {
 		return val, true
 	}
 	if val, found := cfg.ScalarTypeMap[typeName]; found {
-		return val, true
+		_, scalarTypeName := PkgAndType(val)
+		return scalarTypeName, true
 	}
 	return "", false
 }
 
 func (s Source) String() string {
 	return fmt.Sprintf("%s:%d", s.FileName, s.Line)
+}
+
+// take a string in the form github.com/package/foo.Bar and split it into package and type
+func PkgAndType(name string) (string, string) {
+	parts := strings.Split(name, ".")
+	if len(parts) == 1 {
+		return "", name
+	}
+
+	pkgParts := strings.Split(strings.Join(parts[:len(parts)-1], "."), "/")
+
+	typeName := strings.Join([]string{pkgParts[len(pkgParts)-1], parts[len(parts)-1]}, ".")
+
+	return strings.Join(parts[:len(parts)-1], "."), typeName
 }
