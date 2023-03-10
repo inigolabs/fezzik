97b9f7870545f70f7632d3c6329f3780d112d825
Nikolai Kaploniuk <nkaploniuk@inigo.io>
2023-03-10 14:53:07 -0800

    debug option: print response body
    commit-id:56a4939e

--- client/gqlclient.go
+++ client/gqlclient.go
@@ -3,6 +3,8 @@ package client
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io"
 	"net/http"
 
 	jsoniter "github.com/json-iterator/go"
@@ -13,9 +15,18 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 type GQLClient struct {
 	endpoint   string
 	httpClient *http.Client
+	debug      bool
 }
 
-func NewGQLClient(endpoint string, httpClient *http.Client) *GQLClient {
+type Option func(c *GQLClient)
+
+func WithDebug() Option {
+	return func(c *GQLClient) {
+		c.debug = true
+	}
+}
+
+func NewGQLClient(endpoint string, httpClient *http.Client, opts ...Option) *GQLClient {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
 	}
@@ -53,7 +64,17 @@ func (c *GQLClient) Query(ctx context.Context, req *GQLRequest, res *GQLResponse
 	decoder := json.NewDecoder(response.Body)
 	err = decoder.Decode(res)
 	if err != nil {
-		return err
+		if c.debug {
+			body, err := io.ReadAll(response.Body)
+			if err != nil {
+				return fmt.Errorf("Code: %d. Cannot read body.", response.StatusCode)
+			}
+
+			return fmt.Errorf("Code: %d. Body: %s", response.StatusCode, body)
+		}
+
+		return fmt.Errorf("Code: %d. Invalid graphql response.", response.StatusCode)
 	}
+
 	return nil
 }
