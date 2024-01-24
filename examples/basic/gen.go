80af880621e2a655a599209365ad104c077ecc43
Andrii Soluk <isoluchok@gmail.com>
2024-01-24 13:00:14 +0200

    Add a specific(1.16) version to generate examples
    commit-id:6773207b

--- examples/basic/gen.go
+++ examples/basic/gen.go
@@ -1,3 +1,3 @@
 package basic
 
-//go:generate go run github.com/inigolabs/fezzik -config fezzik.yml
+//go:generate go1.16 run github.com/inigolabs/fezzik -config fezzik.yml
--- examples/github/gen.go
+++ examples/github/gen.go
@@ -1,3 +1,3 @@
 package github
 
-//go:generate go run github.com/inigolabs/fezzik -config fezzik.yml
+//go:generate go1.16 run github.com/inigolabs/fezzik -config fezzik.yml
